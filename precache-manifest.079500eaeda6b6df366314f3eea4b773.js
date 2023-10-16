f0ab352d9c0dda455830a20beea199111b4e612a
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2023-10-16 21:02:41 +0300

    Updates

---
+++ precache-manifest.079500eaeda6b6df366314f3eea4b773.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b17a77bfdb5c674b223463f29556b49",
+    "url": "/qm-test-assignment/index.html"
+  },
+  {
+    "revision": "de055708e6468ff48f18",
+    "url": "/qm-test-assignment/static/js/2.a4c843b5.chunk.js"
+  },
+  {
+    "revision": "af43d69498672d2af066",
+    "url": "/qm-test-assignment/static/js/main.36b0119c.chunk.js"
+  },
+  {
+    "revision": "9d80982650b2da0828aa",
+    "url": "/qm-test-assignment/static/js/runtime~main.8d4eac27.js"
+  }
+]);
